a3790b49acc46cecae024a42d86be54ccc93733d
Amrita <amrisingh@expediagroup.com>
2024-10-25 00:14:31 +0530

    Adding info

--- LinkedList/Copy List with Random Pointer.java
+++ LinkedList/Copy List with Random Pointer.java
@@ -1,3 +1,4 @@
+/**
 A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
 
 Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
@@ -12,8 +13,8 @@ val: an integer representing Node.val
 random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
 Your code will only be given the head of the original linked list.
 
-
-Code:
+ Asked in Facebook Amazon Microsoft Bloomberg Google Apple
+*/
 
 
 /*
